7593e0e20ed0f48f4db3d15801ee07515fc40397
jeoung-han <wjdgks4752@gmail.com>
2022-07-11 16:59:13 +0900

    no message

--- sqlRepo/05_GROUPBY&HAVING.sql
+++ sqlRepo/05_GROUPBY&HAVING.sql
@@ -3,7 +3,8 @@
 /*
     GROUP BY
         그룹 기준을 제시할 수 있는 구문
-        여러개의 값들을 하나의 그룹으로 묶어서 처리할 목적으로 사용한다.
+        여러개의 값들을 하나의 그룹으로 묶어서 처리할 목적으로 사용한다.\
+        여러값을 적어서 하나의 그룹으로 묶을 수 있다.
 */
 
 -- 전체 사원을 그룹으로 묶어서 총합을 구한 결과
@@ -59,7 +60,67 @@ FROM EMP
 GROUP BY DEPTNO;
 
 
+/*
+    <집계함수>
+        그룹별 산출한 결과 값의 중간 집계를 계산해주는 함수
+                
+        - CUBE
+        - ROLLUP
+        - GROUPING SETS
+*/
 
+SELECT DEPTNO, JOB, SUM(SAL)
+FROM EMP
+--GROUP BY DEPTNO, JOB
+GROUP BY CUBE(DEPTNO, JOB)
+--GROUP BY ROLLUP(DEPTNO, JOB)
+ORDER BY DEPTNO
+;
+
+SELECT DEPTNO, JOB, SUM(SAL)
+FROM EMP
+GROUP BY GROUPING SETS(DEPTNO, JOB)
+ORDER BY DEPTNO
+;
+
+SELECT DEPTNO, JOB, GROUPING(JOB), GROUPING(DEPTNO)
+FROM EMP
+GROUP BY GROUPING SETS(DEPTNO, JOB)
+ORDER BY DEPTNO
+;
+
+SELECT
+    DEPTNO 부서구분,
+    SUM(SAL),
+--    JOB,
+    CASE
+        WHEN GROUPING(JOB) = 1 THEN '모든직업'
+        ELSE JOB
+    END 직업구분,
+    GROUPING(JOB)
+FROM EMP
+GROUP BY ROLLUP(DEPTNO, JOB)
+ORDER BY 부서구분
+;
+
+
+/*
+    <집합 연산자>
+        여러개의 쿼리문을 가지고 하나의 쿼리문으로 만드는 연산자이다.
+        
+        UNION     : 합집합 : 두 쿼리문의 수행 결과를 합친 후, 중복 제거
+        UNION ALL : 합집합 : 두 쿼리문의 수행 결과를 합침. (중복 허용)
+        INTERSECT : 교집합 : 두 쿼리문 수행 결과 중복된 결과만 추출
+        MINUS     : 차집합 : 선행 쿼리의 결과 중 후행 쿼리의 결과값을 뺀 나머지
+*/
+
+SELECT EMPNO, ENAME, DEPTNO, SAL
+FROM EMP
+WHERE SAL > 2000
+MINUS
+SELECT EMPNO, ENAME, DEPTNO, SAL
+FROM EMP
+WHERE SAL > 3000;
 
 
 
